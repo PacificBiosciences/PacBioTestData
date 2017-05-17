1fa1fea04dfbb485825303ec35e5e2d2857327c5
natechols <nathaniel.echols@gmail.com>
2017-05-17 10:01:56 -0700

    fix path
    Former-commit-id: af929867c897b3c4c341936fc5bc55d5f63b86e2

--- bamboo_test.sh
+++ bamboo_test.sh
@@ -4,9 +4,10 @@
 mkdir -p tmp
 /opt/python-2.7.9/bin/python /mnt/software/v/virtualenv/13.0.1/virtualenv.py tmp/venv
 source tmp/venv/bin/activate
-alias pbvalidate='/pbi/dept/secondary/builds/links/current_develop_smrttools-incremental_installdir/smrtcmds/bin/pbvalidate'
-make test
 make install
 pbdata show
 pbdata path
 pbdata get subreads-sequel
+
+export PATH=/pbi/dept/secondary/builds/links/current_develop_smrttools-incremental_installdir/smrtcmds/bin:$PATH
+make test
